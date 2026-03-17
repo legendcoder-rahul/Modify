af9df68c1b8c24e5eb08f17fe96701223839d1c1
Rahul <rahul251035@gmail.com>
2026-03-17 13:59:14 +0530

    fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,21 +1,32 @@
 require('dotenv').config()
+const path = require('path');
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 
-
 const app = express()
+
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
 
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
+// Routes
 const authRoutes = require('./routes/auth.routes')
 const songRoutes = require('./routes/song.routes')
 
 app.use('/api/auth', authRoutes)
 app.use('/api/songs', songRoutes)
+
+const publicPath = path.join(__dirname,'..','public'); 
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 module.exports = app
