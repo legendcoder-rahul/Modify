a0e191f04ac242a93cc3797bc86c8a2ee78199d4
Rahul <rahul251035@gmail.com>
2026-03-17 13:10:21 +0530

    error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-const path = require('path')
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
@@ -8,16 +7,10 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true
-// }))
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 const authRoutes = require('./routes/auth.routes')
 const songRoutes = require('./routes/song.routes')
